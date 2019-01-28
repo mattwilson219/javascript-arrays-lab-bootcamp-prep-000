25d982ff13a70d500cdf9e668ab0453e75f5ed64
Matthew Wilson <wilsonmatt219@gmail.com>
2019-01-28 22:27:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var kittens = ["Milo" , "Otis" , "Garfield"]; 
 
-function destructivelyAppendKitten(name)
+function destructivelyAppendKitten(name) {
 kittens.push(name);
-return kittens
-
+return kittens;
+}
 // Add your functions and code here
